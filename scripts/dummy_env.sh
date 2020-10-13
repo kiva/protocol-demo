79ecbca152c38c4b7c2dd3512ca94a53094b9481
Jacob <jsaur@kiva.org>
2020-10-13 14:48:15 +0000

    improvements to scripts and readmes (#4)
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- scripts/dummy_env.sh
+++ scripts/dummy_env.sh
@@ -3,6 +3,8 @@
 cp aries-guardianship-agency/dummy.env aries-guardianship-agency/.env
 cp aries-key-guardian/dummy.env aries-key-guardian/.env
 cp protocol-gateway/dummy.env protocol-gateway/.env
-cp guardian-bio-auth/dummy.env guardian-bio-auth/.env
 cp protocol-demo/implementations/demo/dummy.env protocol-demo/implementations/demo/.env
-cp protocol-demo/integration_tests/dummy.env protocol-demo/integration_tests/sl/.env 
+cp protocol-demo/integration_tests/dummy.env protocol-demo/integration_tests/.env 
+cd guardian-bio-auth
+./scripts/useDummyEnvFiles.sh
+cd ..
--- scripts/simple_start.sh
+++ scripts/simple_start.sh
@@ -4,7 +4,7 @@
 set -ev
 
 docker-compose -f ../aries-guardianship-agency/docker-compose.yml up -d
-sleep 5
+sleep 10
 docker-compose -f implementations/demo/docker-compose.yml up -d
-sleep 15
+sleep 25
 docker exec -it demo-controller ts-node -r dotenv/config /www/implementations/demo/scripts/setup.demo.simple.ts
