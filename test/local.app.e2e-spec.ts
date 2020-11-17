8167c64fa6594885e000c19ed6d7c94d5fbcc701
Jacob Saur <jsaur@kiva.org>
2020-11-17 15:32:20 -0800

    fix tests
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- test/local.app.e2e-spec.ts
+++ test/local.app.e2e-spec.ts
@@ -1,16 +1,14 @@
 import request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { AppService } from '../src/app/app.service';
-import { AppController } from '../src/app/app.controller';
+import { AppModule } from '../src/app/app.module';
 
 describe('AppController (e2e)', () => {
     let app: INestApplication;
 
     beforeAll(async () => {
         const moduleFixture = await Test.createTestingModule({
-        providers: [AppService],
-        controllers: [AppController],
+            imports: [ AppModule ]
         }).compile();
 
         app = moduleFixture.createNestApplication();
