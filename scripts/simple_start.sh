1ed369c15dec6c8059c8fa73164dbaa86bfbc9e4
Jacob Saur <jsaur@kiva.org>
2020-11-17 15:27:43 -0800

    update
    Signed-off-by: Jacob Saur <jsaur@kiva.org>

--- scripts/simple_start.sh
+++ scripts/simple_start.sh
@@ -4,7 +4,7 @@
 set -ev
 
 docker-compose -f ../aries-guardianship-agency/docker-compose.yml up -d
-sleep 15
+sleep 30 # wait for agency to be up
 docker-compose -f docker-compose.yml up -d
-sleep 25
+sleep 25 # wait for agent to spin up
 docker exec -it demo-controller npm run script:dev /www/src/scripts/setup.demo.ts
